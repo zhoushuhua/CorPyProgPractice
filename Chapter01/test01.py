286852e50a6a2d9f2e6a25f62fcd2da7a21f29a6
A0049 <zhoush049@youdaigc.com>
2018-01-17 16:50:00 +0800

    Chapter01

--- Chapter01/test01.py
+++ Chapter01/test01.py
@@ -1,3 +1,4 @@
+# -*- coding: cp936 -*-
 import re
 
 # m  = re.match("foo", "foo")
@@ -93,7 +94,7 @@ if m is not None:
 """
 
 """
-# ÈúÄË¶ÅÊ∑ªÂä†ÔºüË°®Á§∫ÈùûË¥™Â©™ÂåπÈÖç
+# –Ë“™ÃÌº”£ø±Ì æ∑«Ã∞¿∑∆•≈‰
 patt = "\w+@(\w+\.)?\w+.com"
 print(re.match(patt, "nobody@xxx.com").group())
 print(re.match(patt, "nobody@www.xxx.com").group())
@@ -115,6 +116,7 @@ if m is not None:
     print(m.groups())
 """
 
+"""
 m = re.match("ab", "ab")
 print(m.group())
 print(m.groups())
@@ -135,3 +137,125 @@ print(m.group())
 print(m.group(1))
 print(m.group(2))
 print(m.groups())
+"""
+
+"""
+m = re.search("^The", "The end.")
+if m is not None:
+    print(m.group())
+
+m = re.search("^The", "end. The") # ≤ª∆•≈‰
+if m is not None:
+    print(m.group())
+
+m = re.search(r"\bthe", "bite the dog")
+if m is not None:
+    print(m.group())
+
+m = re.search(r"\bthe", "bitethe dog") # ≤ª∆•≈‰
+if m is not None:
+    print(m.group())
+
+m = re.search(r"\Bthe", "bitethe dog")
+if m is not None:
+    print(m.group())
+"""
+
+"""
+print(re.findall("car", "car"))
+print(re.findall("car", "scary"))
+print(re.findall("car", "carry the barcardi to the car"))
+"""
+
+s = "This and that."
+"""
+print(re.findall(r"(th\w+) and (th\w+)", s, re.I))
+print(re.finditer(r"(th\w+) and (th\w+)", s, re.I).next().groups())
+print(re.finditer(r"(th\w+) and (th\w+)", s, re.I).next().group(1))
+print(re.finditer(r"(th\w+) and (th\w+)", s, re.I).next().group(2))
+print([g.groups() for g in re.finditer(r"(th\w+) and (th\w+)", s, re.I)])
+"""
+"""
+print(re.findall(r"(th\w+)", s, re.I))
+it = re.finditer(r"(th\w+)", s, re.I)
+g = it.next()
+print(g.groups())
+print(g.group(1))
+g = it.next()
+print(g.groups())
+print(g.group(1))
+print([g.group(1) for g in re.finditer(r"(th\w+)", s, re.I)])
+"""
+"""
+print(re.sub("X", "Mr. Smith", "attn: X\n\nDear X,\n"))
+print(re.subn("X", "Mr. Smith", "attn: X\n\nDear X,\n"))
+print(re.sub("X", "Mr. Smith", "attn: X\n\nDear X,\n"))
+print(re.sub("[ae]", "X", "abcdef"))
+print(re.subn("[ae]", "X", "abcdef"))
+"""
+
+"""
+# »’∆⁄∏Ò ΩªØ
+print(re.sub(r"(\d{1,2})/(\d{1,2})/(\d{2}|\d{4})", r"\2/\1/\3", "2/20/91"))
+print(re.sub(r"(\d{1,2})/(\d{1,2})/(\d{2}|\d{4})", r"\2/\1/\3", "2/20/1991"))
+"""
+
+# print(re.split(":", "str1:str2:str3"))
+
+"""
+DATA = (
+    "Mountain View, CA 94040",
+    "Sunnyvale, CA",
+    "Los Altos, 94023",
+    "Cupertino 95014",
+    "Palo Alto CA"
+    )
+
+for datumn in DATA:
+    print(re.split(", |(?= (?:\d{5}|[A-Z]{2})) ", datumn))
+"""
+
+# print(re.findall(r"(?i)yes", "yes? Yes. YES!!"))
+# print(re.findall(r"(?i)th\w+", "The quickest way is through this tunnel."))
+# print(re.findall(r"(?im)(^th[\w ]+)", """
+# This line is the first,
+# another line,
+# that line, it's the best
+# """))
+# print(re.findall(r"th.+", """
+# The first line
+# the second line
+# the third line
+# """))
+# print(re.findall(r"(?s)th.+", """
+# The first line
+# the second line
+# the third line
+# """))
+
+##print(re.search(r"""(?x)
+##\((\d{3})\) # «¯∫≈
+##[ ]         # ø’∞◊∑˚
+##(\d{3})     # «∞◊∫
+##-           # ∫·œﬂ
+##(\d{4})     # ÷’µ„ ˝◊÷
+##""", "(800) 555-1212").groups())
+
+##print(re.findall(r"http://(?:\w+\.)*(\w+\.com)", "http://google.com http://www.google.com http://code.google.com"))
+##print(re.sub(r"\((?P<areacode>\d{3})\) (?P<prefix>\d{3})-(?:\d{4})", "(\g<areacode> \g<prefix>-xxxx)", "(800) 555-1212"))
+
+##print(bool(re.match(r"\((?P<areacode>\d{3})\) (?P<prefix>\d{3})-(?P<number>\d{4}) (?P=areacode)-(?P=prefix)-(?P=number) 1(?P=areacode)(?P=prefix)(?P=number)",
+##              "(800) 555-1212 800-555-1212 18005551212")))
+
+##print(bool(re.match(r"""(?x)
+### match (800) 555-1212, save areacode,prefix,no
+##\((?P<areacode>\d{3})\)[ ](?P<prefix>\d{3})-(?P<number>\d{4})
+### space
+##[ ]
+### match 800-555-1212
+##(?P=areacode)-(?P=prefix)-(?P=number)
+### space
+##[ ]
+### match 18005551212
+##1(?P=areacode)(?P=prefix)(?P=number)""",
+##"(800) 555-1212 800-555-1212 18005551212")))
